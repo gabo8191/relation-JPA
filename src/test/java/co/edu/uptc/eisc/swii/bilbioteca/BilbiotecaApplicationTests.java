ea274d95a5c938874e77bdb15c5abfee7ded159d
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2025-09-20 17:47:15 -0500

    test(app): add application test class

---
+++ src/test/java/co/edu/uptc/eisc/swii/bilbioteca/BilbiotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.eisc.swii.bilbioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbiotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
