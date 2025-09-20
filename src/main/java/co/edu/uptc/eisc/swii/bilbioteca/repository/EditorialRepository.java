f6e832dcfbaa529e55217b2245ae797831bce83e
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2025-09-20 17:46:43 -0500

    feat(repository): add Editorial repository

---
+++ src/main/java/co/edu/uptc/eisc/swii/bilbioteca/repository/EditorialRepository.java
@@ -0,0 +1,10 @@
+
+package co.edu.uptc.eisc.swii.bilbioteca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uptc.eisc.swii.bilbioteca.domain.Editorial;
+
+public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
+
+}
